38f6bf147c31886a9bd689be8b25dd8522d55b96
Tu TV <tutv95@gmail.com>
2015-02-23 00:14:32 +0700

    v2 : Flip Clock

--- js/app.v2.js
+++ js/app.v2.js
@@ -1,31 +1,18 @@
 /**
- * Created by Tu on 22/2/2015.
+ * Created by Tu on 23/2/2015.
  */
 
 var dayOfWeek = ["Chủ nhật", "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy"];
 var linkAPI = "https://script.google.com/macros/s/AKfycbyd5AcbAnWi2Yn0xhFRbyzS4qMq1VucMVgVvhul5XqS9HkAyJY/exec?tz=Asia/Ho_Chi_Minh";
 
-var now = new Date(0);
 $.getJSON(linkAPI, function (data) {
     if (data.status == "ok") {
         now = new Date(data.year, data.month, data.day, data.hours, data.minutes, data.seconds, data.millis);
+        clock = $(".clock").FlipClock(now, {
+            clockFace: 'TwentyFourHourClock'
+        });
     }
     else {
         alert("Xin lỗi! Đã có lỗi xảy ra :[");
     }
 });
-
-function incrementTime() {
-    now = new Date(now.getTime() + 1000);
-}
-
-function loadTime() {
-    $("#hour").text(now.getHours());
-    $("#minute").text(now.getMinutes());
-    $("#second").text(now.getSeconds());
-    $("#dayofweek").text(dayOfWeek[now.getDay()]);
-    $("#date").text(now.getDate() + "/" + now.getMonth() + "/" + now.getFullYear());
-
-    incrementTime();
-    setTimeout("loadTime()", 1000);
-}
